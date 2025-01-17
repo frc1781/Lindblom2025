c612dde5983a47bc4ccde377d0601508064d5f33
Allison <67673392+Allybe@users.noreply.github.com>
2025-01-17 09:40:30 -0600

    Some feedforward work

--- Lindblom2025/src/main/java/tech/lindblom/subsystems/elevator/Elevator.java
+++ Lindblom2025/src/main/java/tech/lindblom/subsystems/elevator/Elevator.java
@@ -9,9 +9,12 @@ import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkClosedLoopController.ArbFFUnits;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.ClosedLoopConfig;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
 
+import edu.wpi.first.math.controller.ElevatorFeedforward;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj.Timer;
 import tech.lindblom.subsystems.types.StateSubsystem;
@@ -21,15 +24,20 @@ import java.util.HashMap;
 
 
 public class Elevator extends StateSubsystem {
-    private SparkMax motorRight;
+    private final SparkMax motorRight;
     private RelativeEncoder rightEncoder;
     private SparkClosedLoopController rightPIDController;
 
     private SparkMax motorLeft;
-    private RelativeEncoder leftEncoder;
-    private SparkClosedLoopController leftPIDController;
 
-    private HashMap<ElevatorState, Double> positions = new HashMap<>();
+    private ElevatorFeedforward feedforwardController = new ElevatorFeedforward
+            (Constants.Elevator.ELEVATOR_KS,
+                    Constants.Elevator.ELEVATOR_KG,
+                    Constants.Elevator.ELEVATOR_KV,
+                    Constants.Elevator.ELEVATOR_KA
+            );
+
+    private final HashMap<ElevatorState, Double> positions = new HashMap<>();
 
     public Elevator() {
         super("Elevator", ElevatorState.SAFE);
@@ -41,16 +49,11 @@ public class Elevator extends StateSubsystem {
 
         SparkMaxConfig rightMotorConfig = new SparkMaxConfig();
         rightMotorConfig.idleMode(IdleMode.kBrake);
-        rightMotorConfig.closedLoop.p(1);
-        rightMotorConfig.closedLoop.i(0);
-        rightMotorConfig.closedLoop.d(0);
         rightMotorConfig.smartCurrentLimit(30);
         motorRight.configure(rightMotorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
 
         //Left Elevator Motor
         motorLeft = new SparkMax(Constants.Elevator.LEFT_ELEVATOR_MOTOR, MotorType.kBrushless);
-        leftEncoder = motorLeft.getEncoder();
-        leftPIDController = motorLeft.getClosedLoopController();
         SparkMaxConfig leftMotorConfig = new SparkMaxConfig();
         leftMotorConfig.idleMode(IdleMode.kBrake);
         leftMotorConfig.inverted(true);
@@ -68,14 +71,13 @@ public class Elevator extends StateSubsystem {
 
     @Override
     public boolean matchesState() {
-        return false;
+        return getPosition() == positions.get(getCurrentState());
     }
 
 
     @Override
     public void init() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'init'");
+
     }
 
 
@@ -83,25 +85,30 @@ public class Elevator extends StateSubsystem {
     public void periodic() {
         switch ((ElevatorState) getCurrentState()) {
             case SAFE: 
-            goToPosition(ElevatorState.SAFE);
+            goToPosition(positions.get(ElevatorState.SAFE));
                 break;
             case L1:
-            goToPosition(ElevatorState.L1);
+            goToPosition(positions.get(ElevatorState.L1));
                 break;
             case L2:
-            goToPosition(ElevatorState.L2);
+            goToPosition(positions.get(ElevatorState.L2));
                 break;
             case L3:
-            goToPosition(ElevatorState.L3);
+            goToPosition(positions.get(ElevatorState.L3));
                 break;
             case L4:
-            goToPosition(ElevatorState.L4);
+            goToPosition(positions.get(ElevatorState.L4));
                 break;
         }
     }
 
+    public double getPosition() {
+        return rightEncoder.getPosition();
+    }
+
     public void goToPosition(double position) {
-        
+        double ff = feedforwardController.calculate((getPosition() - position));
+        motorRight.setVoltage(ff);
     }
 
     public enum ElevatorState implements SubsystemState {
