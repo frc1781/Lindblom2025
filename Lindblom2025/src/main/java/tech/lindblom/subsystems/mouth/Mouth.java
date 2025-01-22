e13d9a4e1fc7d8c2aed433da09a3239a5a964ba8
M95000 <mikeb9500@gmail.com>
2025-01-22 17:30:53 -0600

    Update Mouth.java

--- Lindblom2025/src/main/java/tech/lindblom/subsystems/mouth/Mouth.java
+++ Lindblom2025/src/main/java/tech/lindblom/subsystems/mouth/Mouth.java
@@ -10,23 +10,21 @@ import com.revrobotics.spark.config.SparkMaxConfig;
 import edu.wpi.first.math.util.Units;
 import tech.lindblom.subsystems.types.StateSubsystem;
 import tech.lindblom.utils.Constants;
-import tech.lindblom.utils.EnumCollection.OperatingMode;
 
 import java.util.HashMap;
 
 
-public class Mouth extends StateSubsystem{
+public class Mouth extends StateSubsystem {
     private final SparkMax spinMotor;
     private final SparkMax positionMotor;
 
     private final HashMap<MouthState, Double> positions = new HashMap<>();
 
-    public Mouth() [
+    public Mouth() {
         super("Mouth", MouthState.UP);
 
         //Spin Motor
         spinMotor = new SparkMax(Constants.Mouth.SPIN_MOUTH_MOTOR, MotorType.kBrushless);
-
         SparkMaxConfig spinMotorConfig = new SparkMaxConfig();
         spinMotorConfig.idleMode(IdleMode.kCoast);
         spinMotorConfig.smartCurrentLimit(30);
@@ -39,28 +37,42 @@ public class Mouth extends StateSubsystem{
         positionMotorConfig.smartCurrentLimit(30);
         positionMotor.configure(positionMotorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
 
-        positions.put(MouthState.UP, 0);
-        positions.put(MouthState.DOWN, 0);
-    ]
+        positions.put(MouthState.UP, 0.0);
+        positions.put(MouthState.DOWN, 0.0);
+    }
+
 
     @Override
     public boolean matchesState() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'matchesState'");
+        return getPosition() == positions.get(getCurrentState());
     }
+
 
     @Override
     public void init() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'init'");
     }
+
 
     @Override
     public void periodic() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'periodic'");
+        switch ((MouthState) getCurrentState()) {
+            case UP: 
+            goToPosition(positions.get(MouthState.UP));
+                break;
+            case DOWN:
+            goToPosition(positions.get(MouthState.DOWN));
+                break;
+        }
     }
-    
+
+    public double getPosition() {
+        return 0.0;
+    }
+
+    public void goToPosition(double position) {
+
+    }
+
     public enum MouthState implements SubsystemState {
         UP,
         DOWN
