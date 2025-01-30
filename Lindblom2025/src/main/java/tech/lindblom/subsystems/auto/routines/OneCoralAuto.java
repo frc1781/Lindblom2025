720e4fe640336678a66308a2cd50043ab73e86d1
tay78 <tkwilliams-s@cps.edu>
2025-01-30 16:56:06 -0600

    Update OneCoralAuto.java

---
+++ Lindblom2025/src/main/java/tech/lindblom/subsystems/auto/routines/OneCoralAuto.java
@@ -0,0 +1,20 @@
+package tech.lindblom.subsystems.auto.routines;
+
+import tech.lindblom.subsystems.auto.Auto;
+import tech.lindblom.subsystems.auto.AutoRoutine;
+import tech.lindblom.subsystems.auto.AutoStep;
+import tech.lindblom.subsystems.auto.groups.AutoStepGroup;
+import tech.lindblom.subsystems.auto.groups.DependGroup;
+
+public class OneCoralAuto implements AutoRoutine {
+    @Override
+    public AutoStepGroup[] getAutoStepGroups() {
+        return new AutoStepGroup[] {
+                new DependGroup(new AutoStep[] {
+                    new AutoStep(4, Auto.getPathFromName("collectCoral") ),
+                    new AutoStep(3, Auto.getPathFromName("reef")),
+
+                })
+        };
+    }
+}
