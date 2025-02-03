b6d6f398eb52869bef5ace7523169283a0db910d
M95000 <mikeb9500@gmail.com>
2025-02-03 17:20:38 -0600

    Update Robot.java

--- PIDTuner/src/main/java/frc/robot/Robot.java
+++ PIDTuner/src/main/java/frc/robot/Robot.java
@@ -46,17 +46,6 @@ public class Robot extends LoggedRobot {
   @Override
   public void robotPeriodic() {}
 
-  /**
-   * This autonomous (along with the chooser code above) shows how to select between different
-   * autonomous modes using the dashboard. The sendable chooser code works with the Java
-   * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the chooser code and
-   * uncomment the getString line to get the auto name from the text box below the Gyro
-   *
-   * <p>You can add additional auto modes by adding additional comparisons to the switch structure
-   * below with additional strings. If using the SendableChooser make sure to add them to the
-   * chooser code above as well.
-   */
-
   /** This function is called once when teleop is enabled. */
   @Override
   public void teleopInit() {}
