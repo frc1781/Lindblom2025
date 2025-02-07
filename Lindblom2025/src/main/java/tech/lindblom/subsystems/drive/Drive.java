192fca75784ac007b72e98b13af6b17950cc5c57
Keith Van Rhein <kvanrhein@cps.edu>
2025-02-07 14:40:50 -0600

    robot was built incorrectly so all ID's are wrong
    This should fix it.

--- Lindblom2025/src/main/java/tech/lindblom/subsystems/drive/Drive.java
+++ Lindblom2025/src/main/java/tech/lindblom/subsystems/drive/Drive.java
@@ -21,23 +21,38 @@ import tech.lindblom.utils.Constants;
 import tech.lindblom.utils.EnumCollection;
 
 public class Drive extends Subsystem {
-    private final SwerveModule frontLeftModule = new DoubleKrakenSwerveModule("Front Left Module",
+    // private final SwerveModule frontLeftModule = new DoubleKrakenSwerveModule("Front Left Module",
+    //         Constants.Drive.FRONT_LEFT_MODULE_DRIVE_MOTOR,
+    //         Constants.Drive.FRONT_LEFT_MODULE_STEER_MOTOR, Constants.Drive.FRONT_LEFT_MODULE_STEER_ENCODER,
+    //         Constants.Drive.FRONT_LEFT_MODULE_STEER_OFFSET, false);
+    // private final SwerveModule frontRightModule = new DoubleKrakenSwerveModule("Front Right Module",
+    //         Constants.Drive.FRONT_RIGHT_MODULE_DRIVE_MOTOR,
+    //         Constants.Drive.FRONT_RIGHT_MODULE_STEER_MOTOR, Constants.Drive.FRONT_RIGHT_MODULE_STEER_ENCODER,
+    //         Constants.Drive.FRONT_RIGHT_MODULE_STEER_OFFSET, false);
+    // private final SwerveModule backLeftModule = new DoubleKrakenSwerveModule("Back Left Module",
+    //         Constants.Drive.BACK_LEFT_MODULE_DRIVE_MOTOR,
+    //         Constants.Drive.BACK_LEFT_MODULE_STEER_MOTOR, Constants.Drive.BACK_LEFT_MODULE_STEER_ENCODER,
+    //         Constants.Drive.BACK_LEFT_MODULE_STEER_OFFSET, true);
+    // private final SwerveModule backRightModule = new DoubleKrakenSwerveModule("Back Right Module",
+    //         Constants.Drive.BACK_RIGHT_MODULE_DRIVE_MOTOR,
+    //         Constants.Drive.BACK_RIGHT_MODULE_STEER_MOTOR, Constants.Drive.BACK_RIGHT_MODULE_STEER_ENCODER,
+    //         Constants.Drive.BACK_RIGHT_MODULE_STEER_OFFSET, false);
+    private final SwerveModule backRightModule = new DoubleKrakenSwerveModule("Front Left Module",
             Constants.Drive.FRONT_LEFT_MODULE_DRIVE_MOTOR,
             Constants.Drive.FRONT_LEFT_MODULE_STEER_MOTOR, Constants.Drive.FRONT_LEFT_MODULE_STEER_ENCODER,
             Constants.Drive.FRONT_LEFT_MODULE_STEER_OFFSET, false);
-    private final SwerveModule frontRightModule = new DoubleKrakenSwerveModule("Front Right Module",
+    private final SwerveModule backLeftModule = new DoubleKrakenSwerveModule("Front Right Module",
             Constants.Drive.FRONT_RIGHT_MODULE_DRIVE_MOTOR,
             Constants.Drive.FRONT_RIGHT_MODULE_STEER_MOTOR, Constants.Drive.FRONT_RIGHT_MODULE_STEER_ENCODER,
             Constants.Drive.FRONT_RIGHT_MODULE_STEER_OFFSET, false);
-    private final SwerveModule backLeftModule = new DoubleKrakenSwerveModule("Back Left Module",
+    private final SwerveModule frontRightModule = new DoubleKrakenSwerveModule("Back Left Module",
             Constants.Drive.BACK_LEFT_MODULE_DRIVE_MOTOR,
             Constants.Drive.BACK_LEFT_MODULE_STEER_MOTOR, Constants.Drive.BACK_LEFT_MODULE_STEER_ENCODER,
             Constants.Drive.BACK_LEFT_MODULE_STEER_OFFSET, true);
-    private final SwerveModule backRightModule = new DoubleKrakenSwerveModule("Back Right Module",
+    private final SwerveModule frontLeftModule = new DoubleKrakenSwerveModule("Back Right Module",
             Constants.Drive.BACK_RIGHT_MODULE_DRIVE_MOTOR,
             Constants.Drive.BACK_RIGHT_MODULE_STEER_MOTOR, Constants.Drive.BACK_RIGHT_MODULE_STEER_ENCODER,
             Constants.Drive.BACK_RIGHT_MODULE_STEER_OFFSET, false);
-
     // Odometry & Kinematics
     public final SwerveDriveKinematics swerveDriveKinematics = new SwerveDriveKinematics(Constants.Drive.FRONT_LEFT_MODULE_POSITION,
             Constants.Drive.FRONT_RIGHT_MODULE_POSITION, Constants.Drive.BACK_LEFT_MODULE_POSITION,
