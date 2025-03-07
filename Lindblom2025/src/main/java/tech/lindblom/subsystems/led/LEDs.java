15760d1a387cc1295712bb5269ae5ac7e6601ed2
Keith Van Rhein <kvanrhein@cps.edu>
2025-03-06 18:02:06 -0600

    flashing LEDs

--- Lindblom2025/src/main/java/tech/lindblom/subsystems/led/LEDs.java
+++ Lindblom2025/src/main/java/tech/lindblom/subsystems/led/LEDs.java
@@ -4,8 +4,14 @@ import tech.lindblom.control.RobotController;
 import tech.lindblom.subsystems.drive.DriveController;
 import tech.lindblom.subsystems.types.StateSubsystem;
 import tech.lindblom.utils.EnumCollection.OperatingMode;
+
+import java.util.TreeMap;
+
+import com.fasterxml.jackson.databind.ser.BeanSerializer;
+
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj.Timer;
 
 public class LEDs extends StateSubsystem {
     private final int LED_LENGTH = 75 + 75;
@@ -13,15 +19,19 @@ public class LEDs extends StateSubsystem {
 
     private AddressableLED mLedController = null;
     private AddressableLEDBuffer mLedBuffer = null;
+    private int mRainbowFirstPixelHue = 1;
+    private Timer flashingTimer;
+    private boolean flashAlt;
 
     public LEDs(RobotController _robotController) {
-        super("LEDs", LEDState.OPERATING_COLOR);
+        super("LEDs", LEDState.RAINBOW);
         this.robotController = _robotController;
     }
 
-
     @Override
     public void init() {
+        flashingTimer = new Timer();
+        flashingTimer.reset();
         if (mLedController == null) {
             mLedController = new AddressableLED(1);
             mLedBuffer = new AddressableLEDBuffer(LED_LENGTH + 1);
@@ -30,10 +40,40 @@ public class LEDs extends StateSubsystem {
             mLedController.setData(mLedBuffer);
             mLedController.start();
         }
+
+        switch (currentOperatingMode) {
+            case DISABLED:  
+                setState(LEDState.RAINBOW);
+                break;
+            default:
+                break;
+        }
     }
 
     @Override
     public void periodic() {
+        switch((LEDState) getCurrentState()) {
+            case RAINBOW:
+                rainbow();
+            case WHITE:
+                solid(255, 255, 255);
+                break;
+            case RED:
+                solid(255, 0, 0);
+                break;
+            case GREEN:
+                solid(0, 255, 0);
+                break;
+            case BLUE:
+                solid(0, 0, 255);
+                break;
+            case SYNC:
+                flashing(0, 255, 0);
+                break;
+            case OVER:
+                flashing(255, 255, 0);
+                break;
+        }
   
         if ((mLedBuffer == null || mLedController == null)) {
             return;
@@ -62,12 +102,40 @@ public class LEDs extends StateSubsystem {
         }
     }
 
+    private void flashing(int r, int g, int b) {
+        if (flashingTimer.get() > 0.2) {
+            if (flashAlt) {
+                solid(0, 0, 0);
+            } else {
+                solid(r, g, b);
+            }
+            flashingTimer.reset();
+            flashingTimer.stop();
+            flashAlt = !flashAlt;
+        }
+
+        flashingTimer.start();
+    }
+
+    private void rainbow() {
+        for (var i = 0; i < mLedBuffer.getLength(); i++) {
+
+            final var hue = (mRainbowFirstPixelHue + (i * 180 / mLedBuffer.getLength())) % 180;
+            mLedBuffer.setHSV(i, hue, 255, 128);
+        }
+
+        mRainbowFirstPixelHue += 3;
+
+        mRainbowFirstPixelHue %= 180;
+    }
+
+
     @Override
     public boolean matchesState() {
         return this.getCurrentState() != LEDState.EXPECTED_FAIL;
     }
 
     public enum LEDState implements SubsystemState{
-        OPERATING_COLOR, WHITE, RED, GREEN, BLUE, EXPECTED_FAIL
+        RAINBOW, SYNC, WHITE, RED, GREEN, BLUE, OVER, EXPECTED_FAIL
     }
 }
