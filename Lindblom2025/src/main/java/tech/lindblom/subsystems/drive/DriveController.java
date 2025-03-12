ea2e5a3abf79b41ac0198ec3626d35611e20c1f7
Allison <67673392+Allybe@users.noreply.github.com>
2025-03-12 16:37:35 -0500

    faster scoring

--- Lindblom2025/src/main/java/tech/lindblom/subsystems/drive/DriveController.java
+++ Lindblom2025/src/main/java/tech/lindblom/subsystems/drive/DriveController.java
@@ -231,30 +231,16 @@ public class DriveController extends StateSubsystem {
                 return inputSpeeds;
         }
 
-
-        if (Math.abs(targetOffset - cameraOffset) > Constants.Drive.OFFSET_TOLERANCE 
-            && robotController.isElevatorInPoleState()
+        if (robotController.isElevatorInPoleState()
                 && robotController.isArmInPoleState()
-                && areValidCameraReading(cameraOffset) 
-                && robotController.getCenteringSide() != ReefCenteringSide.CENTER) {
-            inputSpeeds.vyMetersPerSecond = centeringYawController.calculate(cameraOffset, targetOffset);
-        } else {
+                && robotController.getCenteringSide() != ReefCenteringSide.CENTER)
+        {
             if (apriltagId == -1) {
                 inputSpeeds.vyMetersPerSecond = EEUtil.clamp(-0.2, 0.2, getCurrentState() == DriverStates.CENTERING_RIGHT ? -0.2 : 0.2);
             } else {
-                inputSpeeds.vyMetersPerSecond = 0;
+                inputSpeeds.vyMetersPerSecond = centeringYawController.calculate(cameraOffset, targetOffset);
             }
         }
-
-//        if (Math.abs(targetOffset - cameraOffset) > Constants.Drive.OFFSET_TOLERANCE
-//            && areValidCameraReading(cameraOffset)
-//            && robotController.getCenteringSide() == ReefCenteringSide.CENTER) {
-//                if (apriltagId == -1) {
-//                    inputSpeeds.vyMetersPerSecond = EEUtil.clamp(-0.5, 0.5, 0.005 * (targetOffset - cameraOffset));
-//                } else {
-//                    inputSpeeds.vyMetersPerSecond = 0;
-//                }
-//            }
 
         double leftTOFDistance = leftTOF.getRange();
         double rightTOFDistance = rightTOF.getRange();
