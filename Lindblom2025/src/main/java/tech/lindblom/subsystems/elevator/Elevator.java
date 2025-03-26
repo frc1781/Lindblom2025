4576e2ab53031d5633995d7badf4602a1a8cf580
Keith Van Rhein <kvanrhein@cps.edu>
2025-03-25 21:04:34 -0500

    also trying stall and free limits
    Not sure if this makes sense but worth a try.  Can also add RPMs as the indication of a break off other than zero for stall versus free limit.

--- Lindblom2025/src/main/java/tech/lindblom/subsystems/elevator/Elevator.java
+++ Lindblom2025/src/main/java/tech/lindblom/subsystems/elevator/Elevator.java
@@ -65,7 +65,7 @@ public class Elevator extends StateSubsystem {
 
         SparkMaxConfig rightMotorConfig = new SparkMaxConfig();
         rightMotorConfig.idleMode(IdleMode.kCoast);
-        rightMotorConfig.smartCurrentLimit(10);
+        rightMotorConfig.smartCurrentLimit(10, 20);
         motorRight.configure(rightMotorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
 
         //Left Elevator Motor
@@ -73,9 +73,8 @@ public class Elevator extends StateSubsystem {
         SparkMaxConfig leftMotorConfig = new SparkMaxConfig();
         leftMotorConfig.idleMode(IdleMode.kCoast);
         leftMotorConfig.follow(motorRight, true);
-        leftMotorConfig.smartCurrentLimit(30);
+        leftMotorConfig.smartCurrentLimit(10, 20);
         motorLeft.configure(leftMotorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-
 
         positions.put(ElevatorState.POLE, new Double[]{750.0, minSecondStageDistance});
         positions.put(ElevatorState.SAFE, new Double[]{minFirstStageDistance, 80.0});
